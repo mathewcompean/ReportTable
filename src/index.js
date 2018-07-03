f8d6da89ea981d669654702a114b5cf30d2935ba
Matthew Compean <matthew.compean@pixelogicmedia.com>
2018-07-03 16:18:28 -0700

    Added all filters and sorters. Tweaked table size. Added function to create a filter with unique keys (no repeating values)

--- src/index.js
+++ src/index.js
@@ -2,19 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import './index.css';
-import { Table, Badge } from 'antd';
+import { Table } from 'antd';
 
 class ReportTable extends React.Component {
+
+  ReportTable(data){
+    this.data = data;
+  }
+
+  data = [];
+
   state = {
     filteredInfo: null,
     sortedInfo: null,
   };
   handleChange = (pagination, filters, sorter) => {
-    console.log('Various parameters', pagination, filters, sorter);
-    this.setState({
-      filteredInfo: filters,
-      sortedInfo: sorter,
-    });
+    console.log('parameters', pagination, filters, sorter);
   }
   clearFilters = () => {
     this.setState({ filteredInfo: null });
@@ -34,47 +37,26 @@ class ReportTable extends React.Component {
     });
   }
 
-  createFilterList(){
+  createUniqueFilterList(property){
+    var list = [];
+    var values = [];
 
+    for(var i = 0; i < this.data.length; i++){
+      var record = this.data[i];
+      var prop = record[property];
+
+      if(!values.includes(prop)){
+        list.push({text: prop, value: prop});
+        values.push(prop);
+      }
+    }
+
+    return list;
   }
   
   render() {
-    let { sortedInfo, filteredInfo } = this.state;
-    sortedInfo = sortedInfo || {};
-    filteredInfo = filteredInfo || {};
-    
-    const mainCols = [
-      { title: 'Title', dataIndex: 'title', key: 'title',fixed: 'left', 
-      filters: [
-          { text: 'A', value: 'A -' },
-          { text: 'B', value: 'B -' },
-        ],
-        filteredValue: filteredInfo.name || null,
-        onFilter: (value, record) => record.name.includes(value),
-        sorter: (a, b) => a.name.length - b.name.length,
-        sortOrder: sortedInfo.columnKey === 'name' && sortedInfo.order,width: 120},
-      { title: 'Aplha', dataIndex: 'alpha', key: 'alpha', fixed: 'left', width: 100},
-      { title: 'Line of Business', dataIndex: 'lineOfBusiness', key: 'lineOfBusiness', width: 120 },
-      { title: 'Line of Service', dataIndex: 'lineOfService', key: 'lineOfService', width: 120 },
-      { title: 'Start Date', dataIndex: 'startDate1', key: 'startDate1', width: 120},
-      { title: 'Completion Date', dataIndex: 'completionDate1', key: 'completionDate1', width: 120 },
-      { title: 'Jop Type', dataIndex:'jobType', key: 'jobType', width: 120},
-      { title: 'Due Date (Actual)', dataIndex: 'dueDate', key: 'dueDate', width: 120 },
-      { title: 'Assets', dataIndex: 'assets', key: 'assets', width: 120},
-      { title: 'Workability Date (Target)', dataIndex: 'workabilityDate', key: 'workabilityDate', width: 120 },
-      { title: 'Start Date', dataIndex: 'startDate2', key: 'startDate2', width: 120 },
-      { title: 'Completion Date', dataIndex: 'completionDate2', key: 'completionDate2', width: 120 },
-      { title: 'Assignee', dataIndex: 'assignee', key: 'assignee', width: 120 },
-      { title: 'Pixelogic Manager', dataIndex: 'pxlManager', key: 'pxlManager', width: 120 },
-      { title: 'Client Manager', dataIndex: 'clientManager', key: 'clientManager', width: 120 },
-      { title: 'Client Group', dataIndex: 'clientGroup', key: 'clientGroup', width: 120 },
-      { title: 'Pixelogic Notes', dataIndex: 'pxlNotes', key: 'pxlNotes', width: 120 },
-      { title: 'Client Notes', dataIndex: 'clientNotes', key: 'clientNotes', width: 120 }
-    ];
-  
-    const data = [];
-    for (let i = 0; i < 2; ++i) {
-      data.push({
+    for (let i = 0; i < 4; ++i) {
+      this.data.push({
         key: i,
         title: 'A - Test Title',
         alpha: '12345',
@@ -94,16 +76,126 @@ class ReportTable extends React.Component {
         clientGroup: 'Test Client Group',
         pxlNotes: 'Some PLM notes',
         clientNotes: 'Some Client notes',
+        jobID: "22222",
+        batchID: "002211",
+        poNum: "120732073290"
+      });
+      this.data.push({
+        key: i + 20,
+        title: 'B - Test Title',
+        alpha: '78901',
+        lineOfBusiness: 'Service',
+        lineOfService: 'Business',
+        startDate1: '2014-12-20 23:12:00',
+        completionDate1: '2014-12-24 23:12:00',
+        jobType: 'Encoding',
+        dueDate: '2014-12-30 23:12:00',
+        assets: 'Some Assets',
+        workabilityDate: '2014-12-19 23:12:00',
+        startDate2: '2014-12-20 23:12:00',
+        completionDate2: '2014-12-22 23:12:00',
+        assignee: 'Test Worker',
+        pxlManager: 'Test PXL Manager',
+        clientManager: 'Test Client Manager',
+        clientGroup: 'Test Client Group',
+        pxlNotes: 'Some PLM notes',
+        clientNotes: 'Some Client notes',
+        jobID: "22222",
+        batchID: "002211",
+        poNum: "120732073290"
       });
     }
+
+    var titleFilterList = this.createUniqueFilterList("title");
+    var alphaFilterList = this.createUniqueFilterList("alpha");
+    var lineOfBusinessFilterList = this.createUniqueFilterList("lineOfBusiness");
+    var lineOfServiceFilterList = this.createUniqueFilterList("lineOfService");
+    var assigneeFilterList = this.createUniqueFilterList("assignee");
+    var pxlManagerFilterList = this.createUniqueFilterList("pxlManager");
+    var clientManagerFilterList = this.createUniqueFilterList("clientManager");
+    var clientGroupFilterList = this.createUniqueFilterList("clientGroup");
+    var jobIDFilterList = this.createUniqueFilterList("jobID");
+    var batchIDFilterList = this.createUniqueFilterList("batchID");
+    var poNumFilterList = this.createUniqueFilterList("poNum");
+    
+    const mainCols = [
+      { title: 'Title', dataIndex: 'title', key: 'title', 
+        filters: titleFilterList,
+        onFilter: (value, record) => record.title.indexOf(value) === 0,
+        sorter: (a, b) => a.title.length - b.title.length,
+        width: 120},
+      { title: 'Alpha', dataIndex: 'alpha', key: 'alpha',
+        filters: alphaFilterList,
+        onFilter: (value, record) => record.alpha.indexOf(value) === 0,
+        sorter: (a, b) => parseInt(a.alpha, 10) - parseInt(b.alpha, 10),
+       width: 120},
+      { title: 'Line of Business', dataIndex: 'lineOfBusiness', key: 'lineOfBusiness',
+        filters: lineOfBusinessFilterList,
+        onFilter: (value, record) => record.lineOfBusiness.indexOf(value) === 0,
+        width: 120 },
+      { title: 'Line of Service', dataIndex: 'lineOfService', key: 'lineOfService',
+        filters: lineOfServiceFilterList,
+        onFilter: (value, record) => record.lineOfService.indexOf(value) === 0,
+        width: 120 },
+      { title: 'Start Date', dataIndex: 'startDate1', key: 'startDate1', 
+        sorter: (a, b) => a.startDate1.length - b.startDate1.length,
+        width: 120},
+      { title: 'Completion Date', dataIndex: 'completionDate1', key: 'completionDate1', 
+        sorter: (a, b) => a.completionDate1.length - b.completionDate1.length,
+        width: 120 },
+      { title: 'Jop Type', dataIndex:'jobType', key: 'jobType', width: 120},
+      { title: 'Due Date (Actual)', dataIndex: 'dueDate', key: 'dueDate', 
+        sorter: (a, b) => a.dueDate.length - b.dueDate.length,
+        width: 120 },
+      { title: 'Assets', dataIndex: 'assets', key: 'assets', width: 120},
+      { title: 'Workability Date (Target)', dataIndex: 'workabilityDate', key: 'workabilityDate', 
+        sorter: (a, b) => a.workabilityDate.length - b.workabilityDate.length,
+        width: 120 },
+      { title: 'Start Date', dataIndex: 'startDate2', key: 'startDate2', 
+        sorter: (a, b) => a.startDate2.length - b.startDate2.length,
+        width: 120 },
+      { title: 'Completion Date', dataIndex: 'completionDate2', key: 'completionDate2',
+        sorter: (a, b) => a.completionDate2.length - b.completionDate2.length,
+        width: 120 },
+      { title: 'Assignee', dataIndex: 'assignee', key: 'assignee', 
+        filters: assigneeFilterList,
+        onFilter: (value, record) => record.assignee.indexOf(value) === 0,
+        width: 120 },
+      { title: 'Pixelogic Manager', dataIndex: 'pxlManager', key: 'pxlManager', 
+        filters: pxlManagerFilterList,
+        onFilter: (value, record) => record.pxlManager.indexOf(value) === 0,
+        width: 120 },
+      { title: 'Client Manager', dataIndex: 'clientManager', key: 'clientManager', 
+        filters: clientManagerFilterList,
+        onFilter: (value, record) => record.clientManager.indexOf(value) === 0,
+        width: 120 },
+      { title: 'Client Group', dataIndex: 'clientGroup', key: 'clientGroup', 
+        filters: clientGroupFilterList,
+        onFilter: (value, record) => record.clientGroup.indexOf(value) === 0,
+        width: 120 },
+      { title: 'Pixelogic Notes', dataIndex: 'pxlNotes', key: 'pxlNotes', width: 120 },
+      { title: 'Client Notes', dataIndex: 'clientNotes', key: 'clientNotes', width: 120 },
+      { title: 'Job ID', dataIndex: 'jobID', key: 'jobID', 
+        filters: jobIDFilterList,
+        onFilter: (value, record) => record.jobID.indexOf(value) === 0,
+        width: 120 },
+      { title: 'Batch ID', dataIndex: 'batchID', key: 'batchID', 
+        filters: batchIDFilterList,
+        onFilter: (value, record) => record.batchID.indexOf(value) === 0,
+        width: 120 },
+      { title: 'PO #', dataIndex: 'poNum', key: 'poNum', 
+        filters: poNumFilterList,
+        onFilter: (value, record) => record.poNum.indexOf(value) === 0,
+        width: 120 },
+    ];
 
     return (
         <Table 
         className="embedded-report-table"
         columns={mainCols}
-        dataSource={data} 
+        dataSource={this.data} 
         onChange={this.handleChange} 
-        scroll={{x: 4000, y:1000}}
+        scroll={{x: 4200, y:1000}}
         />
 
     );
